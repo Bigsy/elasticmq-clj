b1c685a263723bdce1b80ed8bf8041dbc1b81fdc
Bill Hedworth <bill.hedworth@gmail.com>
2022-12-20 16:32:59 +0000

    custom test

---
+++ test/elasticmq_clj/custom_test.clj
@@ -0,0 +1,32 @@
+(ns elasticmq-clj.custom-test
+  (:require [clojure.test :refer :all]
+            [elasticmq-clj.core :as sut])
+  (:import (com.amazonaws.client.builder AwsClientBuilder AwsClientBuilder$EndpointConfiguration)
+           [com.amazonaws.services.sqs
+            AmazonSQSClientBuilder]
+           [com.amazonaws.auth
+            BasicAWSCredentials
+            AWSStaticCredentialsProvider]))
+
+(use-fixtures :once sut/with-elasticmq-fn)
+
+(defn around-all
+  [f]
+  (sut/with-elasticmq-fn (.getPath (clojure.java.io/resource "custom.conf")) f))
+
+(use-fixtures :once around-all)
+
+(def client
+  (let [creds (new BasicAWSCredentials "x" "x")
+        reg (new AwsClientBuilder$EndpointConfiguration "http://localhost:9321" "us-east-1")]
+    (-> (AmazonSQSClientBuilder/standard)
+        (.withEndpointConfiguration reg)
+        (.withCredentials (new AWSStaticCredentialsProvider creds))
+        (.build))))
+
+(deftest can-wrap-around
+  (testing "using defaults"
+     (-> client
+         (.createQueue "wibble"))
+     (is (= "{QueueUrls: [http://localhost:9321/000000000000/wibble],}" (str (-> client .listQueues))))))
+
